396731385504d68eaa535763fca0eba9ec9521fa
ww <344541946@qq.com>
2019-10-25 11:08:59 +0800

    lambda

---
+++ JIANYA/src/main/java/com/example/demo/model/Person.java
@@ -0,0 +1,15 @@
+package com.example.demo.model;
+
+import lombok.Data;
+
+@Data
+public class Person {
+    private String name;
+
+    private Integer age;
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+}
